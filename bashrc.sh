a4c25ee3e9b78387d83565b396f95d427a230095
Maxim Orlovsky <370007@niuitmo.ru>
2023-07-13 15:22:14 +0300

    Improved bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -1,5 +1,14 @@
 dev_dir=~/dev
 
+alias c='xclip'
+alias v='xclip -o'
+
+to () {
+    cd "$@"
+    p=$(pwd -P)
+    cd "${p}"
+}
+
 export PATH="$PATH:$HOME/apps/go/bin"
 export PATH="$PATH:$HOME/apps/nvim/bin"
 export PATH="$PATH:$HOME/apps/ziglang/bin"

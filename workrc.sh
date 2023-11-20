6edbf18d20e9529056cfad22b93ff48bf0854e78
Orlovsky Maxim <orlovskiy.maxim@huawei-partners.com>
2023-11-20 16:20:12 +0300

    Updated workrc.sh

--- workrc.sh
+++ workrc.sh
@@ -2,7 +2,7 @@ dev_dir=~/dev
 arkc_dir="$dev_dir/arkcompiler"
 core_dir="$arkc_dir/runtime_core/static_core"
 ets_frontend_dir="$dev_dir/arkcompiler/ets_frontend"
-ets2panda_dir="$ets_frontend_dir/ets2panda"
+es2panda_dir="$ets_frontend_dir/ets2panda"
 ets_plugin_dir="$core_dir/plugins/ets"
 
 sandbox_dir=$dev_dir/sandbox
@@ -10,13 +10,28 @@ sandbox_dir=$dev_dir/sandbox
 cmake_arkc() {
     echo "Executing with parameters: " 
     echo "$@"
+    # -DPANDA_WITH_BENCHMARKS=true \
     cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DPANDA_CROSS_COMPILER=false "$@"
 }
 
-cmake_ets2panda() {
-    echo "Executing with parameters: " 
-    echo "$@"
-    cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DPANDA_CROSS_COMPILER=false -DPANDA_WITH_TESTS=true -DPANDA_WITH_BENCHMARKS=true "$@"
+cmake_arkc_default() {
+    cmake_arkc $core_dir
+}
+
+cmake_es2panda() {
+    echo "Executing: " 
+    echo "cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang 
+        -DPANDA_CROSS_COMPILER=false -DPANDA_WITH_TESTS=true -DPANDA_WITH_BENCHMARKS=true 
+        -DES2PANDA_PATH='$ets_frontend_dir/es2panda'
+        $@"
+    cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang \
+        -DPANDA_CROSS_COMPILER=false -DPANDA_WITH_TESTS=true \
+        -DES2PANDA_PATH="$es2panda_dir"\
+        "$@"
+}
+
+cmake_es2panda_default() {
+    cmake_es2panda $core_dir
 }
 
 update_repo() {
@@ -25,7 +40,7 @@ update_repo() {
     fi
     arkc_origin="${1%%/*}"
     ecma_origin="${2%%/*}"
-    ets2_origin="${3%%/*}"
+    es2_origin="${3%%/*}"
     cd $4 &&
     git fetch $arkc_origin &&
     git rebase $1 --autostash &&
@@ -33,18 +48,18 @@ update_repo() {
     git fetch $ecma_origin && 
     git rebase $2 --autostash &&
     cd $6 &&
-    git fetch $ets2_origin &&
+    git fetch $es2_origin &&
     git rebase $3 --autostash && cd $4
 }
 
 update_arkc() {
     arkc_branch="origin/master"
     ecma_branch="origin/master"
-    ets2_branch="origin/master"
-    update_repo $arkc_branch $ecma_branch $ets2_branch $core_dir $ets_frontend_dir $ets2panda_dir
+    es2_branch="origin/master"
+    update_repo $arkc_branch $ecma_branch $es2_branch $core_dir $ets_frontend_dir $es2panda_dir
 }
 
-go_arkc() { cd $arkc_dir; }
-go_ets() { cd $ets_frontend_dir; }
-go_core() { cd $core_dir; }
-go_ets_plugin() { cd $ets_plugin_dir; }
+cd_arkc() { cd $arkc_dir; }
+cd_ets() { cd $ets_frontend_dir; }
+cd_core() { cd $core_dir; }
+cd_ets_plugin() { cd $ets_plugin_dir; }

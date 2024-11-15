a7ded9236cf12b67acfa12a0cb0a13d34422f658
Orlovskiy Maksim <m.orlovskiy@vkteam.ru>
2024-11-15 15:52:16 +0300

    removed .zshrc and changed bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,7 @@
 
 alias vim='nvim'
-alias c='xclip'
-alias v='xclip -o'
+# alias c='xclip'
+# alias v='xclip -o'
 alias st='st &>/dev/null &'
 
 to () {
@@ -11,25 +11,23 @@ to () {
 }
 
 # Go in local path
-export PATH="$PATH:/usr/local/go/bin"
+# export PATH="$PATH:/usr/local/go/bin"
 # Dotnet
-export DOTNET_ROOT="$HOME/.dotnet"
-export PATH="$PATH:$HOME/.dotnet"
-export PATH="$PATH:$HOME/.dotnet/tools"
+# export DOTNET_ROOT="$HOME/.dotnet"
+# export PATH="$PATH:$HOME/.dotnet"
+# export PATH="$PATH:$HOME/.dotnet/tools"
 
 # Add all apps to PATH
-for d in $HOME/apps/*/; do
-    export PATH="$PATH:$d"/bin
-done
+# for d in $HOME/apps/*/; do
+#     export PATH="$PATH:$d"/bin
+# done
 
 export dev_dir=~/dev
 sandbox_dir=$dev_dir/sandbox
 sync_dir=$dev_dir/sync
-gitee_sync_dir=$dev_dir/gitee_sync
 
 cd_sync() { cd $sync_dir; }
-cd_sync_gitee() { cd $gitee_sync_dir; }
 
 export PS1='\[\e[32m\]\w\[\e[33m\]$(__git_ps1)\[\e[00m\]$ '
 
-. ~/dev/sync/workrc.sh
+. z.sh
